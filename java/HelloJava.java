2a01c0c6874b22f115634e8781e229dfc9255c0a
upendra-rusm <33053178+upendra-rusm@users.noreply.github.com>
2022-07-24 14:45:43 +0800

    python and java

---
+++ java/HelloJava.java
@@ -0,0 +1,38 @@
+public class HelloJava
+{
+
+    public static void main(String[] args)
+    {
+
+        // condition statement 
+        int normalvar=10;
+
+        int[] age=new int[10]; // size 10 but index only 9 index=(size-1)
+
+        age[0]=1;
+        age[1]=2;
+        
+        System.out.println("age"+age[0]);
+        
+       /* if(age > 18)
+        {
+            System.out.println("adult");
+        }
+        else
+        {
+            System.out.println(" not adult");
+        }
+        */
+
+       /* for(int i=1;i<=10;)
+        {
+            System.out.println(++i); // 1-2 | 4 | 6
+            System.out.println(i++); // 1 -2 (3) | 4 (5)
+        }
+
+        System.out.println("test"); */
+
+    }
+
+
+}
